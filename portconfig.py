0d15dc28e2a3e078f50debb374d603ae0f9a7013
Roel Baardman <roel.baardman@gmail.com>
2017-02-21 11:25:49 +0100

    Pylint portconfig.py

--- portconfig.py
+++ portconfig.py
@@ -15,35 +15,37 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with nettools.  If not, see <http://www.gnu.org/licenses/>.
+"""This file provides basic functionality for patchport configuration"""
 
 import sys
 import socket
 
-#import Patchlist 
 from Cisco import CiscoTelnetSession
 from Cisco import CiscoSet
 
 
 my_fqdn = socket.getfqdn()
-hostname = socket.gethostname()
-dns_domain = my_fqdn[len(hostname):] 
+main_hostname = socket.gethostname()
+dns_domain = my_fqdn[len(main_hostname):]
 telnet_port = 23
 
 #Patchlist stuff
 patchlist_patchid_column = "Patchnr"
 
 #Init global vars
-patchnumber = None
-vlanname = None
-username = None
-password = None
+main_patchnumber = None
+main_vlanname = None
+main_username = None
+main_password = None
 
 def print_list(thelist):
+	"""Concatenate a list of items"""
 	thelist_str = [ str(x) for x in thelist ]
 	output = '\n'.join(thelist_str)
 	print output
 
 def connect_to_switch(hostname, port, username, password):
+	"""Connect to a switch and return the session"""
 	session = CiscoTelnetSession()
 	if not session.open(hostname, port, username, password):
 	       sys.stderr.write("Error connecting to: " + hostname + ":" + str(telnet_port))
@@ -51,6 +53,7 @@ def connect_to_switch(hostname, port, username, password):
 	return session
 
 def fix_patchid(patchid):
+	"""Fix common errors in a patchid"""
 	patchid.replace(".", "-")
 	elements = patchid.split("-")
 	new_elements = []
@@ -61,9 +64,10 @@ def fix_patchid(patchid):
 			corrected_element = element
 		new_elements.append(corrected_element)
 	patchid_corrected = '-'.join(new_elements)
-	return patchid_corrected	
+	return patchid_corrected
 
 def get_available_patchports(hostname, port, username, password):
+	"""Get all available patchports"""
 	switches = CiscoSet(username, password, hostname, port)
 	switches.discover_devices()
 	all_ports = switches.execute_on_all(CiscoTelnetSession.show_interface_vlan)
@@ -72,6 +76,7 @@ def get_available_patchports(hostname, port, username, password):
 
 
 def get_available_vlans(hostname, port, username, password):
+	"""Get all available VLANs"""
 	dynamic_vlan = { 'vlanname' : 'dynamic', 'vlanid' : 'dynamic', 'status' : 'software-generated' }
 	trunk_vlan = { 'vlanname' : 'trunk', 'vlanid' : 'trunk', 'status' : 'software-generated' }
 	try:
@@ -79,18 +84,18 @@ def get_available_vlans(hostname, port, username, password):
 		vlans = session.show_vlan()
 		vlans.append(dynamic_vlan)
 		vlans.append(trunk_vlan)
-		vlans_sorted = sorted(vlans, key=lambda k: k['vlanid'])
 		return vlans
-	except:
+	except: #pylint: disable=bare-except
 		return None
 
 def get_available_vlannames(hostname, port, username, password):
+	"""Extract all VLAN names"""
 	vlans = get_available_vlans(hostname, port, username, password)
 	vlannames = [ { 'vlanname' : x['vlanname'], 'vlanid' : x['vlanid'] } for x in vlans ]
-	vlannames_sorted = sorted(vlannames, key=lambda k : k['vlanid'])
 	return vlannames
 
 def vlanname_to_vlanid(switch_hostname, port, username, password, vlanname):
+	"""Convert a VLAN name to a VLAN id"""
 	vlans = get_available_vlans(switch_hostname, port, username, password)
 	for vlan in vlans:
 		cur_vlanid = vlan["vlanid"]
@@ -100,13 +105,15 @@ def vlanname_to_vlanid(switch_hostname, port, username, password, vlanname):
 	return None
 
 def get_port_from_patchid(hostname, port, username, password, patchid):
+	"""Translate a patchid to a switchport"""
 	patches = get_available_patchports(hostname, port, username, password)
 	for row in patches:
 		if fix_patchid(row["patchid"]) == fix_patchid(patchid):
 			return row
 	return None
-	
-def configure_patchid_raw(username, password, switch_hostname, switchport, vlanid, old_vlanid):
+
+def configure_patchid_raw(username, password, switch_hostname, switchport, vlanid, old_vlanid): #pylint: disable=too-many-arguments
+	"""Perform the raw actions needed to configure a patchid"""
 	print "Going to set switchport %s of switch %s to vlanid %s (previously %s)..." % (switchport, switch_hostname, vlanid, old_vlanid)
 	print "...Connecting to switch"
 	session = connect_to_switch(switch_hostname, telnet_port, username, password)
@@ -120,13 +127,14 @@ def configure_patchid_raw(username, password, switch_hostname, switchport, vlani
 	print "Done"
 
 def configure_patchid(username, password, patchid, vlanname):
+	"""Configure a patchid"""
 	port = get_port_from_patchid(default_switch, telnet_port, username, password, patchid)
 	vlanid = vlanname_to_vlanid(default_switch, telnet_port, username, password, vlanname)
 	switch_hostname = port["hostname"]
 	switchport = port["interface"]
 	old_vlanid = port["vlanid"]
 	configure_patchid_raw(username, password, switch_hostname, switchport, vlanid, old_vlanid)
-	
+
 if __name__ == '__main__':
 	#This block initializes some variables depending on how we were called
 	if len(sys.argv) < 3:
@@ -134,54 +142,38 @@ if __name__ == '__main__':
 		sys.stderr.write("Usage: " + sys.argv[0] + "first-switch username password patchnumber 	to list all available vlans\n")
 		sys.stderr.write("Usage: " + sys.argv[0] + "first-switch username password patchnumber vlan	to change the configuration of a port\n")
 		sys.exit(-1)
-	
-	try:	
+
+	try:
 		default_switch = str(sys.argv[1])
-		username = str(sys.argv[2])
-		password = str(sys.argv[3])
-		patchnumber = str(sys.argv[4])
-		vlanname = str(sys.argv[5])
+		main_username = str(sys.argv[2])
+		main_password = str(sys.argv[3])
+		main_patchnumber = str(sys.argv[4])
+		main_vlanname = str(sys.argv[5])
 	except IndexError:
 		pass
 
 
-	
+
 	#This block determines how we run:
-	if 	username is not None and \
-		password is not None and \
-		patchnumber is None and \
-		vlanname is None:
-		print_list(get_available_patchports(default_switch, telnet_port, username, password))
-	elif 	username is not None and \
-		password is not None and \
-		patchnumber is not None and \
-		vlanname is None:
-		print_list(get_available_vlannames(default_switch, telnet_port, username, password))
-	elif	username is not None and \
-		password is not None and \
-		patchnumber is not None and \
-		vlanname is not None:
-		configure_patchid(username, password, patchnumber, vlanname)
+	if 	main_username is not None and \
+		main_password is not None and \
+		main_patchnumber is None and \
+		main_vlanname is None:
+		print_list(get_available_patchports(default_switch, telnet_port, main_username, main_password))
+	elif 	main_username is not None and \
+		main_password is not None and \
+		main_patchnumber is not None and \
+		main_vlanname is None:
+		print_list(get_available_vlannames(default_switch, telnet_port, main_username, main_password))
+	elif	main_username is not None and \
+		main_password is not None and \
+		main_patchnumber is not None and \
+		main_vlanname is not None:
+		configure_patchid(main_username, main_password, main_patchnumber, main_vlanname)
 	else:
 		sys.stderr.write("AIEEE! This should never happen!\n")
-	
+
 	sys.exit(0)
 
 
-	row = Network.search_patchlist(patchlist_filename, "Patchnr", patchnumber, Network.fix_patchid)
-	
-	#Determine which switch to connect to
-	switch_hostname = row["Switch name"]
-	switch_fqdn = switch_hostname + "." + dns_domain
-	
-	session = CiscoTelnetSession()
-	if not session.open(switch_hostname, telnet_port, username, password):
-		sys.stderr.write("Error connecting to: " + switch_hostname + ":" + str(telnet_port))
-		sys.exit(-1)
-	
-	#Translate vlan name to vlan id
-	vlans = session.show_vlan()
-	print vlans
-	
-	portname = row["Interface"]
-		
+
